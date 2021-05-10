1affcf99efe8b6149ba2c8da9bf37dcac0ce00e1
adarshp007 <adarshmp123@gmail.com>
2021-05-10 17:24:21 +0530

    db changed

--- LoginAuth/settings.py
+++ LoginAuth/settings.py
@@ -78,12 +78,14 @@ WSGI_APPLICATION = 'LoginAuth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'signup',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'signup',
+        # 'USER':'root',
+        # 'PASSWORD':'',
+        # 'HOST':'localhost',
+        # 'PORT':'3306'
     }
 }
 
