0c94f1e4bdd780a70041c945c8fa788fb3794fc9
adarshp007 <adarshmp123@gmail.com>
2021-05-10 17:11:44 +0530

    host config

--- LoginAuth/settings.py
+++ LoginAuth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m&7cx--j^bqh*r864c=oaotq$(a8na&m7#6o@tsb!kk^f44pi9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn LoginAuth.wsgi --log-file -
